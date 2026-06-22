2731d4b284e9b3eff5c2beccfbfd5056ac00055a
Shreya Khosla <khoslashreya2007@gmail.com>
2026-06-22 13:04:31 +0530

    07_Missing_Number.cpp

---
+++ 07_Missing_Number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) 
+    {
+        int n = nums.size();
+        int sum = n*(n+1)/2;
+        int asum= 0;
+        for(int i = 0;i<n;i++){
+            asum+=nums[i];
+        }
+    int missing= sum - asum;
+    return missing;
+    }
+};
