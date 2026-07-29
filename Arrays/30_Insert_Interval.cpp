48bf20c0709970fca531a91e3d77598fa0e01962
Shreya Khosla <khoslashreya2007@gmail.com>
2026-07-29 23:07:14 +0530

    Arrays: Insert Interval

---
+++ Arrays/30_Insert_Interval.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        intervals.push_back(newInterval);
+        sort(intervals.begin(), intervals.end());
+        vector<vector<int>> answer;
+        answer.push_back(intervals[0]);
+        int n = intervals.size();
+        for(int i = 1; i < n; i++)
+        {
+            if(answer.back()[1] >= intervals[i][0])
+            {
+                answer.back()[1] = max(answer.back()[1], intervals[i][1]);
+            }
+            else
+            {
+                answer.push_back(intervals[i]);
+            }
+        }
+        return answer;
+        
+    }
+};
