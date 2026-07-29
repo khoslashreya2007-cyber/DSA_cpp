9dcbe6ed775a60a24d23d4764609d8d1f4ec81c1
Shreya Khosla <khoslashreya2007@gmail.com>
2026-07-29 23:02:47 +0530

    Math: Palindrome Number

---
+++ Arrays/23_palindrome_number.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isPalindrome(int x) 
+    {
+        
+        if (x < 0) {
+            return false;
+        }
+
+        long reverse = 0;
+        long n = x;
+
+        while (x > 0) {
+            reverse = (reverse * 10) + (x % 10);
+            x /= 10;
+        }
+
+        return reverse == n;        
+    }
+};
